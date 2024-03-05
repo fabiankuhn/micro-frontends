32f608e789b01d300f815f3cca1432d7d7d5451d
fabiankuhn <fabian.kuhn@lemonbyte.ch>
2024-03-05 23:58:26 +0100

    Set up module federation

--- shell/src/app/app.routes.ts
+++ shell/src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import {loadRemoteModule} from '@angular-architects/module-federation';
+import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'client',
+        loadComponent: () =>
+            loadRemoteModule({
+                type: 'module',
+                remoteEntry: 'http://localhost:4201/remoteEntry.js',
+                exposedModule: './Component'
+            }).then(m => m.AppComponent)
+    },
+];
